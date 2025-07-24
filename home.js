208e380e8d4c693946562d26da34457c9d3a8d2b
Harini <harini.vanitha25@gmail.com>
2025-07-24 20:55:32 +0530

    portfolio

---
+++ home.js
@@ -0,0 +1,5 @@
+function showdiv(id){
+    document.getElementById('page1').style.display = 'none';
+    document.getElementById('page2').style.display = 'none';
+    document.getElementById(id).style.display = 'block';
+  }
